6874652ad8dce8693d1346a3b81e14a8a8e19fa5
satsmicky <SATSMICKY@GMAIL.COM>
2025-01-11 07:56:31 +0000

    string updated

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,43 @@
+const name = "satish"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // this is string interpolation
+
+
+const gameName = new String ('hiteshhc');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('c'));
+
+
+//****methods of strings to be learned
+
+const newString = gameName.substring(0,4);
+//console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+
+//console.log(anotherString);
+
+const newStringOne = "   satish   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://satish.com/satsmicky"
+// console.log(url.replace('sats', 'satish'))
+
+// console.log(url.includes('sats'))
+
+//********converting into array
+// 
+// 
+
+console.log(gameName.split(''));
+
