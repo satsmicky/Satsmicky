8210536402eb710da0797e070842fac76a29e465
satsmicky <SATSMICKY@GMAIL.COM>
2025-01-11 08:49:36 +0000

    Math function updated

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,31 @@
+const balance = new Number(100)
+// console.log(balance);
+
+const score = 400
+// console.log(score);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+const hundreds = 100000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//*****************maths**************//
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(59));
+// console.log(Math.ceil(4.33));
+// console.log(Math.max(4,3,5,8,6));
+
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min)
+
