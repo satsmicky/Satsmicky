4fe6b0cb47251adcfae060deb2bbecf8ec7b1ed9
satsmicky <SATSMICKY@GMAIL.COM>
2025-01-11 07:05:37 +0000

    Datatype summary

---
+++ 01_basic/dataTypes-summary.js
@@ -0,0 +1,48 @@
+// premitive datatyes
+
+// 7 types: String, Number, Boolean, null (empty), undefined, Symbol, BigInt
+
+
+const score = 100 // JS consider as it given type
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outsideTemp = null
+let userEmail; // this will be undefined
+
+// defining symbol
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+//const bigNumber = 1545484465456n
+
+
+
+
+
+
+
+// Referance: Non Premitive
+
+// Arrays, Objects, Functions
+
+const heros = ["shaktimaan", "Hanuman", "doga"];
+let myObj = {
+    name: "Satish",
+    age: 25,
+    
+}
+
+//console.log(myObj);
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
+console.log(typeof anotherId);
+
+
