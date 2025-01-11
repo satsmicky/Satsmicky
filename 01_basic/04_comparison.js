e88e6d569afd731b56ed665a5d6f45dd08b5ac43
satsmicky <SATSMICKY@GMAIL.COM>
2025-01-11 06:40:24 +0000

    Compariosn

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1 );
+// console.log(2 >= 1 );
+// console.log(2 < 1 );
+// console.log(2 == 1 );
+// console.log(2 != 1 );
+
+// console.log("2" > 1 );
+// console.log("02" == 1 );
+
+// console.log(null > 0 );
+// console.log(null == 0 );
+// console.log(null >= 0 );
+
+// console.log(undefined == 0);
+
+//===  this is a strict check used for legal check
+
+console.log(2 === 2);
+
+
+
