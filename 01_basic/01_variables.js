77b391f1505bcd2e49f1bf3608caaae3008e5f80
satsmicky <SATSMICKY@GMAIL.COM>
2025-01-11 05:19:43 +0000

    dataTyes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "satish@gmail.com"
+var accountPassword = "12345"
+accountCity  = "Pune"
+
+// accountId = 555
+
+console.log(accountId);
+
+console.table({accountId, accountEmail, accountPassword, accountCity})
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) we are using nodejs, not browser
+
+console.log (3+3)
+
+console.log("satish") ///code readability should be high
+
+let name = "satish"
+let age = 15
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to power 53
+// bigint 
+//string => "" should be put in double qoutes
+//boolean => true /false
+//null => standalone value
+// undefined => variable declared but value was not give
+//symbol => unique 
+
+// object 
+
+console.log(typeof undefined)
+console.log(typeof null)
+
+
