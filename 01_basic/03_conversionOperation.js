79505ba8c97e2f3875401274a9a0784a49b44a2f
satsmicky <SATSMICKY@GMAIL.COM>
2025-01-11 05:53:37 +0000

    Conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "true"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33bs" => NaN
+// true  => 1
+// False => 0
+//"Satish" => true
+
+
+let isLoggedIn = "Satish"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
